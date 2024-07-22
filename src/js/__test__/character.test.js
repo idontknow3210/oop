c4c91b865158b957f6fc18eec45a8d6862201248
Yura <bobr515yura@yandex.ru>
2024-07-22 10:45:37 +0300

    work

---
+++ src/js/__test__/character.test.js
@@ -0,0 +1,138 @@
+import Character from '../character.js';
+import Bowman from '../bowman.js';
+import Daemon from '../daemon.js';
+import Magician from '../magician.js';
+import Swordsman from '../swordsman.js';
+import Undead from '../undead.js';
+import Zombie from '../zombie.js';
+
+test('New character with invalid name (too short)', () => {
+    expect(
+        () => new Character('O', 'Bowman', 100, 100),
+    ).toThrow('Invalid name length');
+});
+
+test('New character with invalid name (too long)', () => {
+    expect(
+        () => new Character('LooooooongName', 'Daemon', 100, 100),
+    ).toThrow('Invalid name length');
+});
+
+test('New character with invalid character type', () => {
+    expect(
+        () => new Character('Bowy', 'FakeBowman', 100, 100),
+    ).toThrow('Invalid character type');
+});
+
+test('Inflict damage to character', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.damage(50);
+    expect(testChar).toEqual({
+        name: 'Bowy',
+        type: 'Bowman',
+        health: 62.5,
+        level: 1,
+        attack: 25,
+        defence: 25,
+    });
+});
+
+test('The value of health cannot be negative', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.health = 0;
+    testChar.damage(50);
+    expect(testChar.health).toEqual(0);
+});
+
+test('Go to the next level', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.levelUp();
+    expect(testChar).toEqual({
+        name: 'Bowy',
+        type: 'Bowman',
+        health: 100,
+        level: 2,
+        attack: 30,
+        defence: 30,
+    });
+});
+
+test('Go to the next level with zero health', () => {
+    const testChar = new Character('Bowy', 'Bowman', 25, 25);
+    testChar.health = 0;
+    expect(() => testChar.levelUp()).toThrow(
+        'You cannot raise the level with zero health',
+    );
+});
+
+test('New Bowman', () => {
+    const bowman = new Bowman('Bowy');
+    expect(bowman).toEqual({
+        name: 'Bowy',
+        type: 'Bowman',
+        health: 100,
+        level: 1,
+        attack: 25,
+        defence: 25,
+    });
+});
+
+test('New Daemon', () => {
+    const daemon = new Daemon('Daemy');
+    expect(daemon).toEqual({
+        name: 'Daemy',
+        type: 'Daemon',
+        health: 100,
+        level: 1,
+        attack: 10,
+        defence: 40,
+    });
+});
+
+test('New Magician', () => {
+    const magician = new Magician('Magy');
+    expect(magician).toEqual({
+        name: 'Magy',
+        type: 'Magician',
+        health: 100,
+        level: 1,
+        attack: 10,
+        defence: 40,
+    });
+});
+
+test('New Swordsman', () => {
+    const swordsman = new Swordsman('Swordy');
+    expect(swordsman).toEqual({
+        name: 'Swordy',
+        type: 'Swordsman',
+        health: 100,
+        level: 1,
+        attack: 40,
+        defence: 10,
+    });
+});
+
+test('New Undead', () => {
+    const undead = new Undead('Undy');
+    expect(undead).toEqual({
+        name: 'Undy',
+        type: 'Undead',
+        health: 100,
+        level: 1,
+        attack: 25,
+        defence: 25,
+    });
+});
+
+test('New Zombie', () => {
+    const zombie = new Zombie('Zomy');
+    expect(zombie).toEqual({
+        name: 'Zomy',
+        type: 'Zombie',
+        health: 100,
+        level: 1,
+        attack: 40,
+        defence: 10,
+    });
+});
---
+++ src/js/character.js
@@ -0,0 +1,38 @@
+const types = [
+    'Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie',
+];
+
+export default class Character {
+    constructor(name, type, attack, defence) {
+        if (name.length < 2 || name.length > 10) {
+            throw new Error('Invalid name length');
+        }
+        if (!types.includes(type)) {
+            throw new Error('Invalid character type');
+        }
+        this.name = name;
+        this.type = type;
+        this.health = 100;
+        this.level = 1;
+        this.attack = attack;
+        this.defence = defence;
+    }
+
+    levelUp() {
+        if (this.health > 0) {
+            this.level += 1;
+            this.attack *= 1.2;
+            this.defence *= 1.2;
+            this.health = 100;
+        } else {
+            throw new Error('You cannot raise the level with zero health');
+        }
+    }
+
+    damage(points) {
+        this.health -= points * (1 - this.defence / 100);
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+}
